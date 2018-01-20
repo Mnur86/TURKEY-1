f3dfd1dcec20812b5963dde2f9f6bcad219d8215
mnur86 <mnur_86@icloud.com>
2018-01-20 15:47:19 +0000

    first commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/mnur/pen/gopXRV.
+
+ I'm creating website that describes the beautiful country of Turkey; I'll be talking about places, food and culture.
---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "80px";
+document.getElementsByTagName("h2")[0].style.fontSize = "40px";
+document.getElementsByTagName("h3")[0].style.fontSize = "40px";
+document.getElementsByTagName("h4")[0].style.fontSize = "40px";
